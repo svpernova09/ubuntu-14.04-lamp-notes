a023c484640ca2bf72651e0d09494e6848d18ce3
Svpernova09 <joe@joeferguson.me>
2014-04-17 22:55:32 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Ubuntu 14.04 LAMP Server Notes
+===
+
+To install basic LAMP server run
+
+```
+sh install.sh
+```
---
+++ install.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo apt-get install php5-cli php5-mysql php5-mcrypt php5-sqlite vim screen zsh mysql-server apache2 git
