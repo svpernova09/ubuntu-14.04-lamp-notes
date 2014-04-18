69016aadb472a6dd20a46357575185bbf8cd2959
Svpernova09 <joe@joeferguson.me>
2014-04-17 23:04:41 -0500

    Adding node/npm and list of what gets installed

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ To install basic LAMP server run
 ```
 sh install.sh
 ```
+
+Stuff that gets installed
+===
+
+* PHP 5.5.x
+	* Mcrypt
+	* sqlite support
+
+* Apache 2.4
+* MySQL Server 5.5.x
+* zsh
+* vim
+* Git
+* NodeJS
+* npm
+	* bower (globally)
+	* grunt-cli (globally)
--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-sudo apt-get install php5-cli php5-mysql php5-mcrypt php5-sqlite vim screen zsh mysql-server apache2 git
+sudo apt-get install php5-cli php5-mysql php5-mcrypt php5-sqlite vim screen zsh mysql-server apache2 git nodejs npm
+sudo npm install -g bower grunt-cli
